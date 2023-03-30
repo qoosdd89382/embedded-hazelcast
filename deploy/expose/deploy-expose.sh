c5362d2a9c332b6fcf7e16a258c2fdd4f6f9ac91
cherry <qoosdd89382@gmail.com>
2023-03-30 17:47:31 +0800

    upload deploy yaml and kubectl scripts

---
+++ deploy/expose/deploy-expose.sh
@@ -0,0 +1,1 @@
+kubectl expose deploy embedded-hazelcast --target-port=8888 --port=8888
---
+++ deploy/expose/port-forward.sh
@@ -0,0 +1,2 @@
+# 7777 is localhost entering port, 8888 is service port
+kubectl port-forward svc/embedded-hazelcast 7777:8888
